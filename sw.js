9ffdb9e89f4623fc87a18b1405d8edc69824d87d
ll9-m <2445890457@qq.com>
2026-09-25 16:58:10 +0800

    .

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'noteforge-v9';
+const CACHE = 'noteforge-v10';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg', './fflate.umd.js', './marked.umd.js'];
 
 self.addEventListener('install', (e) => {

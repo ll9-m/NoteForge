20efe8e914b899722bc5b6596c4942accf5e808b
ll9-m <2445890457@qq.com>
2026-09-25 16:45:14 +0800

    小问题修复

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'noteforge-v8';
+const CACHE = 'noteforge-v9';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg', './fflate.umd.js', './marked.umd.js'];
 
 self.addEventListener('install', (e) => {
